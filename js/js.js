09b81c2008fd62cdb2f4b4356bf3d20b6748a744
Sara S Kjeldgaard <sara.kjeldgaard@gmail.com>
2020-09-11 23:15:20 +0200

    File structure

---
+++ js/js.js
@@ -0,0 +1,17 @@
+/*
+ * fil: js.js
+ * purpose: introdction to jQuery
+ */
+console.log('file: js/js.js loaded');
+
+// A $( document ).ready() block.
+$(document).ready(function () { // kører så snart DOM er klar
+
+
+	// ... din kode herfra ...
+    
+
+    
+	// ... din kode slut ...
+
+}); // denne line må ikke slettes

c60af7287fd4de55f8dd60a51accd8bf6d0ece5d
Line <treanii@hotmail.dk>
2020-09-14 11:04:11 +0200

    Scene arrow + back-to-top

--- js/js.js
+++ js/js.js
@@ -10,7 +10,26 @@ $(document).ready(function () { // kører så snart DOM er klar
 
 	// ... din kode herfra ...
     
+/**
+* Back to Top button by Matthew Cain (https://codepen.io/matthewcain/pen/ZepbeR)
+**/
+var btn = $('#button');
 
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+/**
+* END of Back to Top Button by Matthew Cain
+**/
     
 	// ... din kode slut ...
 
